0213c63bef5c53f117eae632cc6b18137d596907
Seth Thompson <sthomp12@iit.edu>
2012-07-03 09:22:52 -0500

    Added app to settings

--- settings.py
+++ settings.py
@@ -120,6 +120,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
    'testproject.books',
+   'testproject.data_log',
 )
 
 # A sample logging configuration. The only tangible logging
