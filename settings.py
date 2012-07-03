6cdbc300c94830fcb147a53ba0da7f1218a17718
Seth Thompson <sthomp12@iit.edu>
2012-07-03 18:17:17 -0500

    added middleware

--- settings.py
+++ settings.py
@@ -113,9 +113,9 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    #'django.contrib.sites',
-    #'django.contrib.messages',
-    #'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
