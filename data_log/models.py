ad4d3c382c22069262c09f5d2f737cd4a2669a28
Seth Thompson <sthomp12@iit.edu>
2012-07-03 09:21:13 -0500

    Added log model

--- data_log/models.py
+++ data_log/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Log(models.Model):
+   date = DateField()
+   author = CharField(max_length=50)
+   system1_food = DecimalField()
+   system2_food = DecimalField()
+   system3_food = DecimalField()
+   system4_food = DecimalField()
+   makeup_added = CharField(max_length=200)
+   temp = DecimalField()
+   ph = DecimalField()
+   do = DecomalField()
+   humidity = DecomalField()
+   note = CharField(max_length=1000)
