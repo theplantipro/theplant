a93a844ce0fccc258c631db081aeecaefed5e56a
Seth Thompson <sthomp12@hawk.iit.edu>
2012-07-03 17:35:20 -0500

    Changed static_root

--- settings.py
+++ settings.py
@@ -1,4 +1,6 @@
 # Django settings for testproject project.
+import os.path
+import sys
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -56,7 +58,9 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+#my add
+PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT,'static') 
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
