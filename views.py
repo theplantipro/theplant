0f5287f769077ad50598e153072b854ae98d9990
Seth Thompson <sthomp12@iit.edu>
2012-07-03 14:52:52 -0500

    Added template

--- views.py
+++ views.py
@@ -1,3 +1,5 @@
+from django.template.loader import get_template
+from django.template import Context
 from django.http import HttpResponse
 import datetime
 
@@ -6,5 +8,6 @@ def hello(request):
 
 def current_datetime(request):
    now = datetime.datetime.now()
-   html = "<html><body>It is now %s.</body></html>" % now 
+   t = get_template('current_datetime.html')
+   html = t.render(Context({'current_date':now})) 
    return HttpResponse(html)
