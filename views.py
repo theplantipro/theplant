26f11e4abed164b28d4c33fa618e5abab28cf687
Seth Thompson <sthomp12@iit.edu>
2012-07-03 15:00:24 -0500

    Added render to response

--- views.py
+++ views.py
@@ -1,6 +1,5 @@
 from django.template.loader import get_template
-from django.template import Context
-from django.http import HttpResponse
+from django.shortcuts import render_to_response
 import datetime
 
 def hello(request):
@@ -8,6 +7,4 @@ def hello(request):
 
 def current_datetime(request):
    now = datetime.datetime.now()
-   t = get_template('current_datetime.html')
-   html = t.render(Context({'current_date':now})) 
-   return HttpResponse(html)
+   return render_to_response('current_datetime.html',{'current_date',now}} 
