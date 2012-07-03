0afe25bfdf687b6c76891994a4232d3d14eba932
Seth Thompson <sthomp12@hawk.iit.edu>
2012-07-03 10:01:15 -0500

    commit

--- views.py
+++ views.py
@@ -6,5 +6,5 @@ def hello(request):
 
 def current_datetime(request):
    now = datetime.datetime.now()
-   html = "<html><body>It is now %s.</body></html>" % now 
+   html = "<html><body>It is now %s.</body></html>" % now
    return HttpResponse(html)
