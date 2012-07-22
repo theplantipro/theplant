0d7e547769a936fb99ae48a8b6ccb18b3aa89b4e
Seth Thompson <sthomp12@iit.edu>
2012-07-22 18:12:12 -0500

    Forgot to import to admin site

--- data_log/admin.py
+++ data_log/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from testproject.data_log.models import Log
+from testproject.data_log.models import Log,Single_Main,Main_Testing,Single_Nutrient
+from testproject.data_log.models import Micro_Nutrient_Testing,Ammonia_Nitrate
+from testproject.data_log.models import Ammonia_Nitrate_Testing
 
 admin.site.register(Log)
 admin.site.register(Single_Main)
