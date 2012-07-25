fe47e1e1549037fa35cdfa49ef8904aa357507c4
Seth Thompson <sthomp12@iit.edu>
2012-07-25 14:15:48 -0500

    Added mn_download to url

--- urls.py
+++ urls.py
@@ -3,7 +3,7 @@ from testproject.views import hello, current_datetime,search_form,search
 from testproject.views import inputs,thanks,download,process,dateedit
 from testproject.views import processdate,edit,plot,start,redirect, mt_inputs
 from testproject.views import mt_edit,mt_download,mt_process,mn_inputs, mn_edit
-from testproject.views import mn_process
+from testproject.views import mn_process,mn_download
 from django.contrib import admin # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 admin.autodiscover()
@@ -23,6 +23,7 @@ urlpatterns = patterns('',
    (r'^app/mn_inputs/thanks/$',thanks),
    (r'^app/download/$',download),
    (r'^app/mt_download/$',mt_download),
+   (r'^app/mn_download/$',mn_download),
    (r'^app/download/process/$',process),
    (r'^app/download/mt_process/$',mt_process),
    (r'^app/download/mn_process/$',mn_process),
