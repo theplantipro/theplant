b4cf324445b153d6b96da2e84e23d4e5e1b81f43
Seth Thompson <sthomp12@iit.edu>
2012-07-25 16:12:03 -0500

    Added am_process to url

--- urls.py
+++ urls.py
@@ -3,7 +3,7 @@ from testproject.views import hello, current_datetime,search_form,search
 from testproject.views import inputs,thanks,download,process,dateedit
 from testproject.views import processdate,edit,plot,start,redirect, mt_inputs
 from testproject.views import mt_edit,mt_download,mt_process,mn_inputs, mn_edit
-from testproject.views import mn_process,mn_download,am_inputs,am_edit,am_download
+from testproject.views import mn_process,mn_download,am_inputs,am_edit,am_download,am_process
 from django.contrib import admin # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 admin.autodiscover()
@@ -30,6 +30,7 @@ urlpatterns = patterns('',
    (r'^app/download/process/$',process),
    (r'^app/download/mt_process/$',mt_process),
    (r'^app/download/mn_process/$',mn_process),
+   (r'^app/download/am_process/$',am_process),
    #(r'^app/download/process/$',plot),
    (r'^app/dateedit/$',dateedit),
    (r'^app/dateedit/processdate/$',processdate),
