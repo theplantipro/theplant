8807c8efb370aaa711b358b3c573684329fb62db
Seth Thompson <sthomp12@iit.edu>
2012-07-26 09:13:17 -0500

    Switched user

--- settings.py
+++ settings.py
@@ -15,8 +15,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'firstDB',                      # Or path to database file if using sqlite3.
-        'USER': 'seth',                      # Not used with sqlite3.
-        'PASSWORD': 'ilikecows',                  # Not used with sqlite3.
+        'USER': 'dbuser',                      # Not used with sqlite3.
+        'PASSWORD': 'dbuser',                  # Not used with sqlite3.
         'HOST': '192.168.1.124',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
